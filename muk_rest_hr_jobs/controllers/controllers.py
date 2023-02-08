56fbab4255b6eb91018243fa68415bbe21fbf34c
Nyx <61583597+mrlittleman@users.noreply.github.com>
2023-02-08 19:02:35 +0800

    updated hrjobs

--- muk_rest_hr_jobs/controllers/controllers.py
+++ muk_rest_hr_jobs/controllers/controllers.py
@@ -2,16 +2,22 @@ import json
 
 from odoo import http
 from odoo.http import request
-
+from openerp.addons.muk_rest.controllers.main import abort, check_token, ensure_db, check_params
+        
 class JobApiController(http.Controller):
-    @http.route('/api/jobs', auth='user', type='http')
-    def job_api(self, **kw):
+    @http.route('/search/<string:model>', auth='none', methods=['GET'], type='http', csrf=False)
+    def job_api(self, model='hr.job', token=None, **kw):
+        check_params({'token': token})
+        check_token()
+        ensure_db()
         try:
-            data = request.env['hr.job']
+            data = request.env[model]
             job_records = data.search([])
             job_data = []
             for record in job_records:
                 job_data.append({'name': record.name, 'id': record.id})
-            return json.dumps(job_data)
-        except Exception:
-            return [{'error': 'Unknown Command' }]
+            return json.dumps(job_data, indent=4)
+        except Exception as error:
+            _logger.error(error)
+            abort({'error': traceback.format_exc()}, rollback=True, status=400)
+
